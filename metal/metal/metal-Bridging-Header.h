30ea0c4b1e4e603c61470a16bdda9bc4dc752e41
sunada-ke <sunada-ke@legendapl.com>
2017-07-27 18:42:21 +0900

    [ADD] Add a sample

---
+++ metal/metal/metal-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  metal-Bridging-Header.h
+//  metal
+//
+//  Created by LaKeel Development Group on 2017/07/27.
+//  Copyright © 2017年 LaKeel Development Group. All rights reserved.
+//
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "ShaderTypes.h"
