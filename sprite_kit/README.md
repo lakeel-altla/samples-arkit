82bd98cf64d78c2c20899c912ce29b35036a04ab
sunada-ke <sunada-ke@legendapl.com>
2017-07-27 18:36:06 +0900

    [ADD] Add a sample

---
+++ sprite_kit/README.md
@@ -0,0 +1,10 @@
+# Description
+This project is a sample of ARKit using SpriteKit.  
+iOS 11 introduces ARKit, a new framework that allows you to easily create unparalleled augmented reality experiences for iPhone and iPad. 
+
+# Required
+
+## Download iOS11 and Xcode9
+Download iOS 11 beta and the latest beta of Xcode 9, which includes the iOS 11 SDK to build AR features in your app.
+[View beta downloads](https://www.google.co.jp/url?sa=t&rct=j&q=&esrc=s&source=web&cd=2&cad=rja&uact=8&ved=0ahUKEwjVh-vMhqnVAhXLzbwKHcl_B_EQFgg2MAE&url=https%3A%2F%2Fdeveloper.apple.com%2Fdownload%2F&usg=AFQjCNHUFtih2Sap3DBGpTzjEFJQqxcrdA)
+
---
+++ sprite_kit/sprite_kit/ViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ViewController.swift
+//  sprite_kit
+//
+//  Created by LaKeel Development Group on 2017/07/27.
+//  Copyright © 2017年 LaKeel Development Group. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import ARKit
+
+class ViewController: UIViewController, ARSKViewDelegate {
+    
+    @IBOutlet var sceneView: ARSKView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and node count
+        sceneView.showsFPS = true
+        sceneView.showsNodeCount = true
+        
+        // Load the SKScene from 'Scene.sks'
+        if let scene = SKScene(fileNamed: "Scene") {
+            sceneView.presentScene(scene)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingSessionConfiguration()
+        
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+    
+    // MARK: - ARSKViewDelegate
+    
+    func view(_ view: ARSKView, nodeFor anchor: ARAnchor) -> SKNode? {
+        // Create and configure a node for the anchor added to the view's session.
+        let node = SKSpriteNode(imageNamed: "hiyoko")
+        node.size = CGSize(width: 50, height: 50)
+        return node
+    }
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+}
